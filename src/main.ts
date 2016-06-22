9edc6e2aba45f895ebd6c8ba7412ed67524c2f1c
Daniel Johnson <navseeker7@gmail.com>
2016-06-21 19:56:47 -0700

    working Firebase 2 version

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,5 @@ if (environment.production) {
 bootstrap(Af2WtfAppComponent, [
   FIREBASE_PROVIDERS,
   // Initialize Firebase app  
-  defaultFirebase({
-    apiKey: "AIzaSyDAjtDsvpHoYLt4PhJzGv4PHDWjHjsb_Nc",
-    authDomain: "af2-wtf.firebaseapp.com",
-    databaseURL: "https://af2-wtf.firebaseio.com",
-    storageBucket: "af2-wtf.appspot.com",
-  })
+  defaultFirebase("https://af2-wtf.firebaseio.com")
 ]);
